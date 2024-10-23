bca2ca8f185ebbbe18c1f78ff388cc92cf963850
GitWolf210800 <ezad821q@hotmail.com>
2024-10-23 20:56:58 -0300

    agregando rutas, componentes y un formulario

--- Documents/proyecto-vue/proyecto-vue/src/components/SideBar.vue
+++ Documents/proyecto-vue/proyecto-vue/src/components/SideBar.vue
@@ -4,6 +4,7 @@
 <h2>Barra Lateral</h2>
 <router-link to="/contador">Contador</router-link>
 <router-link to="/lista-de-tareas">Lista de Tareas</router-link>
+<router-link to="/registrar">Registrar</router-link>
 </div>
 
 </template>
---
+++ Documents/proyecto-vue/proyecto-vue/src/modules/registro/views/RegistrarView.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h2>Formulario de Registro</h2>
+        <Form>
+            <div class="form">
+                <label for="nombre">Nombre:</label>
+                <input type="text" placeholder="ingrese su nombre">
+            </div>
+            <div class="form">
+                <label for="correo">Correo:</label>
+                <input type="email" placeholder="ingrese su email">
+            </div>
+            <div class="form">
+                <button type="submit">Registrar</button>
+            </div>
+        </Form>  
+    </div>
+</template>
+
+<script setup>
+import { Form, Field } from 'vee-validate';
+</script>
+
+<style scoped>
+.form {
+    margin-bottom: 10px;
+}
+</style>
