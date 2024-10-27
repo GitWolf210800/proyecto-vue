7e5c2414ad38891c3c3c7a2e1594b14775a914ca
GitWolf210800 <ezad821q@hotmail.com>
2024-10-27 08:02:08 -0300

    se agrega la funcion watch para escuchar cambios de las props del input

--- Documents/proyecto-vue/proyecto-vue/src/components/Header.vue
+++ Documents/proyecto-vue/proyecto-vue/src/components/Header.vue
@@ -1,11 +1,17 @@
 <template>
     <div class="header">
-        <h3>La temperatura actual es: <Weather /> </h3>
+        {{ mensajeRecibido }} <Weather />
     </div>
 </template>
 
 <script setup>
 import Weather from './Weather.vue';
+import { defineProps, ref, watch } from 'vue';
+const props = defineProps(['mensaje']);
+const mensajeRecibido = ref(props.mensaje);
+watch(()=> props.mensaje, (newValue) => {
+    mensajeRecibido.value = newValue;
+});
 </script>
 
 <style scoped>
@@ -14,7 +20,7 @@ import Weather from './Weather.vue';
     justify-content: center;
     align-items: center;
     padding: 10px 20px;
-    position: fixed;
+    
     top: 0;
     right: 0;
     left: 0;
--- Documents/proyecto-vue/proyecto-vue/src/views/MainView.vue
+++ Documents/proyecto-vue/proyecto-vue/src/views/MainView.vue
@@ -1,8 +1,10 @@
 <template>
-<Header></Header>
+<Header :mensaje="texto" ></Header>
 <div class="container">
     <SideBar />
     <div class="content">
+        <input type="texto" v-model="texto">
+        {{ texto }}
         <router-view></router-view>
     </div>
 </div>
@@ -11,6 +13,9 @@
 <script setup>
 import SideBar from '../components/SideBar.vue';
 import Header from '@/components/Header.vue';
+import { ref } from 'vue';
+
+const texto = ref('');
 </script>
 
 <style>
